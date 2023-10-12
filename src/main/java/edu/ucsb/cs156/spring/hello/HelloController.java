461869a56532009e93645273f5e0d97a3e12a12e
Francisco Leyva <franciscoleyva@umail.ucsb.edu>
2023-10-12 00:07:07 +0000

    updated site with correct github usrname

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from franciscoleyva!";
+        return "Greetings from Francisc0Leyva!";
     }
     
 }
